3792a3e91a03ae7d880756a8b0e2c0ad615679c6
oNora <e.dimchevska@gmail.com>
2023-02-17 09:07:24 +0200

    rename file and folder

--- vue-app-vuex/src/pages/NotFound.vue
+++ vue-app-vuex/src/pages/NotFound.vue
@@ -1,3 +1,21 @@
 <template>
     Not Found
+
+
+    <br>
+    <br>
+    <br>
+    <toggle>
+        <template v-slot="{ greeting }">
+            {{ greeting }}
+        </template>
+    </toggle>
 </template>
+
+<script>
+import Toggle from '../components/test-components/ToggleRenderless.js';
+
+export default {
+    components: {Toggle},
+}
+</script>
